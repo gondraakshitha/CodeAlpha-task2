749879ead40015affbfb69a5358efc32ed2c3440
Akshitha <gondraakshitha@gmail.com>
2026-06-27 23:15:59 +0530

    codealpha internship

---
+++ script.js
@@ -0,0 +1,40 @@
+let display = document.getElementById("display");
+
+function appendValue(value){
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function deleteLast(){
+    display.value = display.value.slice(0,-1);
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "Error";
+    }
+}
+
+// Keyboard Support (Bonus)
+document.addEventListener("keydown", function(event){
+    const key = event.key;
+
+    if((key >= "0" && key <= "9") || "+-*/.".includes(key)){
+        display.value += key;
+    }
+    else if(key === "Enter"){
+        calculate();
+    }
+    else if(key === "Backspace"){
+        deleteLast();
+    }
+    else if(key === "Escape"){
+        clearDisplay();
+    }
+});
